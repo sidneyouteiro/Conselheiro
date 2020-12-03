1359616111679d4515f970702b36fef2560cd54c
Sidney Outeiro <sid@dcc.ufrj.br>
2020-12-03 13:43:41 -0300

    correçao de bugs em scheduler.py

--- conselheiro/scheduler/scheduler.py
+++ conselheiro/scheduler/scheduler.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 from django.core.mail import send_mail
 from django.db.models import Q
 from django.conf import settings
-
+from itertools import chain
 from conselheiro.models import *
 import yahoo_fin.stock_info as si
 from django.utils.dateparse import parse_duration
@@ -30,7 +30,7 @@ def check15min():
         if i % 3 == 0:
             trackings3 = Tracking.objects.all().filter(nome_ativo=ativo.nome_ativo,periodicidade=parse_duration('00:45:00'))
         
-        trackings = trackings1 + trackings2 + trackings3
+        trackings = list(chain(trackings1, trackings2, trackings3))
         for tracking in trackings:
             if tracking.upper_bound < ativo.valor:
                 upper_bound_trigger(tracking.user, ativo.nome_ativo)
@@ -38,19 +38,17 @@ def check15min():
                 lower_bound_trigger(tracking.user,ativo.nome_ativo)
         i = i +1
 
-def upper_bound_trigger(user_id,nome_ativo):
-    user = User.objects.get(pk=user_id)
+def upper_bound_trigger(user,nome_ativo):
     assunto = 'Upper bound {}'.format(nome_ativo)
-    msg = '{}, o valor do ativo {} passou o upper bound estabelecido, é recomendado a venda do ativo'.format(user.usuario, nome_ativo)
+    msg = '{}, o valor do ativo {} passou o upper bound estabelecido, é recomendado a venda do ativo'.format(user.username, nome_ativo)
     enviado_de = settings.EMAIL_HOST_USER
     para_lista = [user.email,]
     send_mail(assunto,msg,enviado_de,para_lista)
 
 
-def lower_bound_trigger(user_id,nome_ativo):  
-    user = User.objects.get(pk=user_id)
+def lower_bound_trigger(user,nome_ativo):  
     assunto = 'Lower bound {}'.format(nome_ativo)
-    msg = '{}, o valor do ativo {} passou o lower bound estabelecido, é recomendado a compra do ativo'.format(user.usuario, nome_ativo)
+    msg = '{}, o valor do ativo {} passou o lower bound estabelecido, é recomendado a compra do ativo'.format(user.username, nome_ativo)
     enviado_de = settings.EMAIL_HOST_USER
     para_lista = [user.email,]
     send_mail(assunto,msg,enviado_de,para_lista)
@@ -58,6 +56,6 @@ def lower_bound_trigger(user_id,nome_ativo):
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(teste15min, 'interval', minutes=1, name='clean_accounts', replace_existing=True)
+    scheduler.add_job(check15min, 'interval', minutes=15, name='clean_accounts', replace_existing=True)
     register_events(scheduler)
     scheduler.start()
