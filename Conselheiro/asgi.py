e60933c711fa635ff88176d03ad183fb3697d9fd
Sidney Outeiro <sid@dcc.ufrj.br>
2021-05-21 02:46:37 -0300

    Renomeando App

--- Conselheiro/asgi.py
+++ Conselheiro/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for InoaDesafio project.
+ASGI config for Conselheiro project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InoaDesafio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Conselheiro.settings')
 
 application = get_asgi_application()
